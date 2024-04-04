e5ead037f4f73954a10d07699329ae93d09822b7
pablogila <pablo.gila.herranz@gmail.com>
2024-04-04 16:57:09 +0200

    lots of updates after the meeting

--- QRotor.py
+++ QRotor.py
@@ -14,12 +14,30 @@ import os
 #          [  1  0  0 -1  2 ]           [                V(4) ]
 
 
+class Variables:
+    def __init__(self):
+        self.comment = None
+        self.runtime = None
+        self.atom_type = None
+        self.constants = None
+        self.set_of_constants = None
+        self.searched_E_levels = None
+        self.potential_name = None
+        self.potential = None
+        self.B = None
+        self.N = None
+        self.x = None
+
+
 class Solutions:
     def __init__(self):
         self.comment = None
+        self.potential = None
+        self.max_potential = None
+        self.min_potential = None
+        self.corrected_offset_potential = None
         self.eigenvalues = None
         self.eigenvectors = None
-        self.max_potential = None
         self.energy_barrier = None
         self.first_transition = None
 
@@ -29,7 +47,8 @@ class Data:
         self.title = None
         self.comment = None
         self.x = None
-        self.set_of_constants = None
+        self.set_of_potentials = None
+        self.set_of_constants = None  # I want to get rid of this
         self.set_of_energies = None
         self.set_of_energies_H = None
         self.set_of_energies_D = None
@@ -38,30 +57,14 @@ class Data:
         self.set_of_eigenvectors_D = None
 
 
-class Variables:
-    def __init__(self):
-        self.comment = None
-        self.runtime = None
-        self.atom_type = None
-        self.constants = None
-        self.set_of_constants = None
-        self.searched_energies = None
-        self.potential = None  # Unused
-        self.m = None
-        self.r = None
-        self.B = None
-        self.N = None
-        self.x = None
-
-
 # Redirect to the desired potential energy function
-def V(x, C):
-    return potential_titov2023(x, C)
-
-
-# Zero potential
-def potential_0(x, C):
-    return 0 * x
+def V(variables:Variables):
+    if variables.potential_name == 'titov2023':
+        return potential_titov2023(variables.x, variables.constants)
+    elif variables.potential_name == 'zero':
+        return potential_zero(variables.x)
+    else:
+        return potential_custom(variables)
 
 
 # Potential energy function of the hindered methyl rotor, from titov2023
@@ -69,66 +72,90 @@ def potential_titov2023(x, C):
     return C[0] + C[1] * np.sin(3*x) + C[2] * np.cos(3*x) + C[3] * np.sin(6*x) + C[4] * np.cos(6*x)
 
 
+# Zero potential
+def potential_zero(x):
+    return 0 * x
+
+
+def potential_custom(variables:Variables):
+    return  # A potential as an array resulting from DFT calculations, etc.
+
+
 # Second derivative matrix, according to the finite difference method
-def second_derivative_matrix(x):
+def laplacian_matrix(x):
     diagonals = [-2*np.ones(len(x)), np.ones(len(x)-1), np.ones(len(x)-1)]
-    second_derivative_matrix = diags(diagonals, [0, -1, 1]).toarray()
+    laplacian_matrix = diags(diagonals, [0, -1, 1]).toarray()
     # Periodic boundary conditions
-    second_derivative_matrix[0, -1] = 1
-    second_derivative_matrix[-1, 0] = 1
+    laplacian_matrix[0, -1] = 1
+    laplacian_matrix[-1, 0] = 1
     dx = x[1] - x[0]
-    second_derivative_matrix /= dx**2
-    return second_derivative_matrix
+    laplacian_matrix /= dx**2
+    return laplacian_matrix
+
+
+def hamiltonian_matrix(variables:Variables):
+    potential = variables.potential
+    B = variables.B
+    x = variables.x
+    H = -B * laplacian_matrix(x) + diags(potential)  # Original Hamiltonian
+    # H = -laplacian_matrix(x) + (1/B)*diags(potential)  # In units of B
+    return H
 
 
 # Solve the Hamiltonian eigenvalues for the time independent Schrödinger equation.
-# Takes a Variables object, returns a Solutions object.
 def solve_energies(variables:Variables):
-    potential = V(variables.x, variables.constants)
-    B = variables.B
-    x = variables.x
-    searched_energies = variables.searched_energies
-    # Solve Hamiltoninan eigenvalues
-    H = -B * second_derivative_matrix(x) + diags(potential)
-    eigenvalues, eigenvectors = eigsh(H, searched_energies, which='SM')
+    potential = V(variables)
+    offset = min(potential)
+    potential = potential - offset
+    variables.potential = potential
+    
+    H = hamiltonian_matrix(variables)
+
+    eigenvalues, eigenvectors = eigsh(H, variables.searched_E_levels, which='SM')
     solutions = Solutions()
+    solutions.potential = potential
+    solutions.max_potential = max(potential)
+    solutions.min_potential = min(potential)
+    solutions.corrected_offset_potential = offset
     solutions.eigenvalues = eigenvalues
     solutions.eigenvectors = eigenvectors
-    solutions.max_potential = max(potential)
     solutions.energy_barrier = max(potential) - min(eigenvalues)
     solutions.first_transition = eigenvalues[1] - eigenvalues[0]
     return solutions
 
 
 # Recurrently solve the energies for a set of potential constants, and print the solutions.
-# Takes a Variables object, returns a Data object with the set of energies and eigenvectors.
-def solve_variables(variables:Variables, out_file):
+def solve_variables(variables:Variables, out_file=None):
     set_of_energies = []
     set_of_eigenvectors = []
+    set_of_potentials = []
     for C in variables.set_of_constants:
         variables.constants = C
         solutions = solve_energies(variables)
-        solutions.comment = f'Potential constants:    {C}'
-        set_of_energies.append(solutions.eigenvalues)
-        # CHECK THIS for set_of_eigenvectors:
-        # I want: [[set_1],[set_2],...]
-        set_of_eigenvectors.append(solutions.eigenvectors)
+        solutions.comment = f'Potential constants:    {C}\n'
         print_solutions(solutions, out_file)
+
+        set_of_energies.append(solutions.eigenvalues)
+        set_of_eigenvectors.append(solutions.eigenvectors)
+        set_of_potentials.append(solutions.potential)
 
     data = Data()
     data.set_of_energies = set_of_energies
     data.set_of_eigenvectors = set_of_eigenvectors
+    data.set_of_potentials = set_of_potentials
 
     return data
 
 
 def print_solutions(solutions:Solutions, out_file=None, print_eigenvectors=False):
-    output = solutions.comment + '\n'
+    output = solutions.comment
+    output += f'Max potential [meV]:    {solutions.max_potential:.4f}\n'
+    output += f'Min potential [meV]:    {solutions.min_potential:.4f}\n'
+    output += f'Corrected offset [meV]: {solutions.corrected_offset_potential:.4f}\n'
     output += f'Eigenvalues [meV]:      '
     for value in solutions.eigenvalues:
         output += f'{value:.4f} '
     output += '\n'
-    output += f'Max potential [meV]:    {solutions.max_potential:.4f}\n'
     output += f'Energy barrier [meV]:   {solutions.energy_barrier:.4f}\n'
     output += f'E1-E0 transition [meV]: {solutions.first_transition:.4f}\n'
     if print_eigenvectors:
@@ -136,6 +163,7 @@ def print_solutions(solutions:Solutions, out_file=None, print_eigenvectors=False
         for value in solutions.eigenvectors:
             output += f'{value}\n'
         output += '\n'
+    output += '\n'
 
     print(output)
     if out_file:
@@ -145,10 +173,11 @@ def print_solutions(solutions:Solutions, out_file=None, print_eigenvectors=False
 
 def print_variables(variables:Variables, out_file=None):
     output = '\n' + variables.comment + '\n'
+    output += f'Potential name:         {variables.potential_name}\n'
     output += f'Atom type:              {variables.atom_type}\n'
     output += f'Inertia B [meV]:        {variables.B:.4f}\n'
     output += f'Grid size N:            {variables.N}\n'
-    output += f'Energy levels computed: {variables.searched_energies}\n'
+    output += f'Energy levels computed: {variables.searched_E_levels}\n'
     output += f'Runtime [s]:            {variables.runtime:.2f}\n'
     output += '\n\n'
     output += '------------------------------------\n\n'
@@ -182,13 +211,13 @@ def plot_energies_and_potentials(data:Data):
     D_linestyle = 'dashed'
     D_label = 'D energies'
 
-    for i, C in enumerate(data.set_of_constants):
+    for i, potential in enumerate(data.set_of_potentials):
         # Plot potential energy
         plt.figure(figsize=(10, 6))
-        plt.plot(data.x, V(data.x, C), color=V_color, label=V_label)
+        plt.plot(data.x, potential, color=V_color, label=V_label)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.title(f'{title} (#' + str(data.set_of_constants.index(C)+1) + ')' )
+        plt.title(f'{title} (#' + str(i+1) + ')' )
         plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
                    ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
         # Plot default set_of_energies
@@ -238,18 +267,18 @@ def plot_eigenvectors(data:Data, levels=None, squared=False, scaling_factor=1):
         eigenvector_label = 'Eigenvect '
         square = 1
     
-    for i, C in enumerate(data.set_of_constants):
+    for i, potential in enumerate(data.set_of_potentials):
 
         # Transpose the 2D array so that each inner array represents a different eigenvector
         eigenvectors_transposed = np.transpose(data.set_of_eigenvectors[i])
 
         # Plot potential energy
         plt.figure(figsize=(10, 6))
-        plt.plot(data.x, V(data.x, C), color=V_color, label=V_label)
+        plt.plot(data.x, potential, color=V_color, label=V_label)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.title(f'{title} (#' + str(data.set_of_constants.index(C)+1) + ')' )
-        if len(data.set_of_constants) == 1:
+        plt.title(f'{title} (#' + str(i+1) + ')' )
+        if len(data.set_of_potentials) == 1:
             plt.title(f'{title}')
         plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
                    ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])
@@ -283,22 +312,28 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 out_file = os.path.join(script_dir, filename)
 
 
-# Instantiate objects. These are used to pass data between functions.
-variables = Variables()
-
 # Atomic masses
 m_H = 1.00784      # H mass
 m_D = 2.014102     # D mass
+# Methyl rotor radius
+r = 0.537  # in meV  # 1.035 angstroms for MAI... CHECK UNITS
+# Inertia. Should be 0.574 for H, according to titov2023
+B_Hydrogen = 1.0 / (2 * 3*(m_H * r**2))
+B_Deuterium = 1.0 / (2 * 3*(m_D * r**2))
+
 
 # Potential constants from titov2023 [C1, C2, C3, C4, C5]
-constants_1 = [
+constants_zero = [
+    [0,0,0,0,0]
+    ]
+constants_titov_1 = [
     [2.7860, 0.0130,-1.5284,-0.0037,-1.2791],
     [2.6507, 0.0158,-1.4111,-0.0007,-1.2547],
     [2.1852, 0.0164,-1.0017, 0.0003,-1.2061],
     [5.9109, 0.0258,-7.0152,-0.0168, 1.0213],
     [1.4526, 0.0134,-0.3196, 0.0005,-1.1461]
     ]
-constants_2 = [
+constants__titov_2 = [
     [2.7860, 0.0130,-1.5284,-0.0037,-1.2791],
     [3.0720, 0.0166,-1.8427,-0.0029,-1.2515],
     [2.7770, 0.0100, 1.7292,-0.0131,-0.9675],
@@ -308,15 +343,18 @@ constants_2 = [
     ]
 
 
+# Instantiate objects. These are used to pass data between functions.
+variables = Variables()
+
+
 # Choose the set of constants to use
-variables.set_of_constants = constants_1
+variables.set_of_constants = constants_titov_1
+variables.potential_name = 'titov2023'  # 'titov2023' or 'zero'
 # Number of energy levels to calculate
-variables.searched_energies = 5
+variables.searched_E_levels = 5
 # Grid size
 variables.N = 100
 variables.x = np.linspace(0, 2*np.pi, variables.N)
-# Methyl rotor radius
-variables.r = 0.62  # meV  # 1.035 angstroms for MAI
 
 
 ####################################
@@ -324,10 +362,12 @@ variables.r = 0.62  # meV  # 1.035 angstroms for MAI
 ####################################
 
 
+# WE CAN JUST SOLVE IT ONCE, AND MULTIPLY BY B. WE NEED TO TEST IT.
+
+
 # Solve for HYDROGEN and print the results
 variables.atom_type = 'H'
-variables.m = m_H
-variables.B = 1.0 / 2 * 3*(variables.m * variables.r**2)  # Inertia, 0.574 From titov2023
+variables.B = B_Hydrogen
 
 time_start = time.time()
 data_H = solve_variables(variables, out_file)
@@ -338,8 +378,7 @@ print_variables(variables, out_file)
 
 # Change the atom type to DEUTERIUM and solve again
 variables.atom_type = 'D'
-variables.m = m_D
-variables.B = 1.0 / 2 * 3*(variables.m * variables.r**2)
+variables.B = B_Deuterium
 
 time_start = time.time()
 data_D = solve_variables(variables, out_file)
@@ -354,6 +393,7 @@ print(f'Data saved to {filename}\n')
 # Group H and D data in the same object, to plot them together
 data = Data()
 data.title = 'Hindered methyl rotor potential'
+data.set_of_potentials = data_H.set_of_potentials  # Both are the same
 data.set_of_energies_H = data_H.set_of_energies
 data.set_of_eigenvectors_H = data_H.set_of_eigenvectors
 data.set_of_energies_D = data_D.set_of_energies
@@ -367,5 +407,5 @@ plot_energies_and_potentials(data)
 data.title = 'Hindered methyl rotor eigenvalues'
 data.set_of_energies = data.set_of_energies_H
 data.set_of_eigenvectors = data.set_of_eigenvectors_H
-plot_eigenvectors(data, [0,1,2], True, 100)
+plot_eigenvectors(data, [0,1,2,3,4], True, 100)
 
