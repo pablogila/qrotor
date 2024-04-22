c41ea283c3544824224fab7ca958fcb7999439dd
Pablo Gila-Herranz <pablo.gila.herranz@gmail.com>
2024-04-22 16:47:05 +0200

    ready to reproduce the results from titov2023

---
+++ scripts/titov2023.py
@@ -0,0 +1,25 @@
+import qrotor as qr
+import numpy as np
+from math import sqrt
+import os
+
+
+# Optimal gridsize value found in the convergence test
+gridsize = 200000
+
+file = qr.logdirfile
+
+variables = qr.Variables()
+variables.potential_name = 'titov2023'
+variables.set_of_constants = qr.constants_titov2023
+variables.B = qr.B_Hydrogen
+variables.atom_type = 'H'
+variables.searched_E_levels = 5
+variables.gridsize = gridsize
+variables.grid = np.linspace(0, 2*np.pi, gridsize)
+variables.comment = f'Energies from titov2023. Reproduced with QRotor, gridsize {gridsize}'
+
+data = qr.solve.energies(variables, file)
+
+qr.file.compress(file)
+
